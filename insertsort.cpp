4f0dd3df85dc082eb1cd72570d82a58e5e796b1f
Zachary Horine <horine.zachary@gmail.com>
2018-10-02 11:45:31 -0700

    Add read from file

--- insertsort.cpp
+++ insertsort.cpp
@@ -5,3 +5,43 @@
 
 
 using namespace std;
+int main(){
+  //print unsorted list
+  printArray(intarr,intarrsize[0]);
+}
+int* fileInput(int* intarrsize){
+  int* intarr;
+  int intcounter = 0;
+
+  ifstream inFile;  //set up the file reader and attempt to read the file
+  inFile.open("data.txt");
+  if (!inFile) {  //make sure the file exists
+    cout << "Unable to open data.txt";
+    exit(1);   // EXIT PROGRAM
+  }
+  char* xarr = new char[10];
+  char x;
+  int xcounter = 0;
+  bool first = true;
+  while (inFile >> x) { //open the file and read every character
+    xarr[xcounter] = x; //add the character read to the buffer
+    xcounter++;
+    if (inFile.peek() < 48 || inFile.peek() > 57){  //check to see if the next character is a space, if so, terminate the number
+      int newnum = atoi(xarr);  //convert the read characters into an integer
+      if (first){ //Check if this is the first number, and then set up the array
+        first = false;
+        intarr = new int[newnum];
+        intarrsize[0] = newnum;
+      }
+      else {  //Add the new number to the integer array
+        intarr[intcounter] = newnum;
+        intcounter++;
+      }
+      xarr = new char[10];  //wipe the input array
+      xcounter = 0;
+    }
+  }
+  inFile.close(); //Close the file
+
+  return intarr;
+}
