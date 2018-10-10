85af32473f33441b760cb71fdcaded4f0b1a41c4
Zachary Horine <horine.zachary@gmail.com>
2018-10-10 02:54:22 -0700

    Rewrite file input

--- stoogesort.cpp
+++ stoogesort.cpp
@@ -3,8 +3,17 @@
 #include <cstring>
 #include <sstream>
 #include <stdlib.h>
+#include <vector>
+
 using namespace std;
+
+vector<int*> fileInput(int* intarrsize);
 void printArray(int* arr, int arrsize);
+
+int main(){
+  int* intarrsize = new int[20]; //storage for sizes of arrays
+  vector<int*> intarrarr = fileInput(intarrsize); //get vector containing arrays to be sorted
+  cout << intarrarr.size()<<endl;
 void printArray(int* arr, int arrsize){ //takes in one array, and prints out all of the elements
   cout << arr[0];
   for (int i = 1; i < arrsize; i++){
@@ -12,3 +21,30 @@ void printArray(int* arr, int arrsize){ //takes in one array, and prints out all
   }
   cout << endl;
 }
+vector<int*> fileInput(int* intarrsize){
+  vector<int*> intarrarr;
+
+  ifstream inFile;  //set up the file reader and attempt to read the file
+  inFile.open("data.txt");
+  if (!inFile) {  //make sure the file exists
+    cout << "Unable to open data.txt";
+    exit(1);   // EXIT PROGRAM
+  }
+  string line;
+  while(getline(inFile,line)){ //gets one line
+    stringstream lineStream(line);
+    string cell;
+    vector<string> parsedRow;
+    while(getline(lineStream,cell,' ')){  //parses line and puts numbers into string vector
+      parsedRow.push_back(cell);
+    }
+    int* temp = new int[stoi(parsedRow.at(0))]; //set length of array
+    intarrsize[intarrarr.size()] = stoi(parsedRow.at(0)); //save length of array
+    for (int i = 1; i < parsedRow.size(); i++){ //add rest of values to the array
+      temp[i-1] = stoi(parsedRow.at(i));
+    }
+    intarrarr.push_back(temp);  //push the array onto the array vector
+  }
+  return intarrarr;
+}
+  }
