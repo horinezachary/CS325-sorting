4c2499682263f5d761d89e1853e8e41544372311
Zachary Horine <horine.zachary@gmail.com>
2018-10-10 02:57:07 -0700

    Add stooge sort

--- stoogesort.cpp
+++ stoogesort.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <sstream>
 #include <stdlib.h>
+#include <cmath>
 #include <vector>
 
 using namespace std;
@@ -10,11 +11,28 @@ using namespace std;
 vector<int*> fileInput(int* intarrsize);
 void fileOutput(vector<int*> intarrarr, int* arrsize);
 void printArray(int* arr, int arrsize);
+void stoogeSort(int* arr, int start, int end);
 
 int main(){
   int* intarrsize = new int[20]; //storage for sizes of arrays
   vector<int*> intarrarr = fileInput(intarrsize); //get vector containing arrays to be sorted
   cout << intarrarr.size()<<endl;
+  for (int arrnum = 0; arrnum < intarrarr.size(); arrnum++){  //complete sort for each array
+    //print num elements
+    cout << "Elements: " << intarrsize[arrnum] << endl;
+
+    //print unsorted list
+    printArray(intarrarr.at(arrnum),intarrsize[arrnum]);
+
+    stoogeSort(intarrarr.at(arrnum),0,intarrsize[arrnum]-1);
+
+    //print sorted list
+    printArray(intarrarr.at(arrnum),intarrsize[arrnum]);
+  }
+  //write sorted lists to file
+  fileOutput(intarrarr,intarrsize);
+}
+
 void printArray(int* arr, int arrsize){ //takes in one array, and prints out all of the elements
   cout << arr[0];
   for (int i = 1; i < arrsize; i++){
@@ -64,4 +82,18 @@ void fileOutput(vector<int*> intarrarr, int* arrsize){
   }
   outfile.close();  //close the file
 }
+
+void stoogeSort(int* arr, int start, int end){
+  if (arr[start] > arr[end]){ //if there are two values left, make sure they are in order
+    //cout << "SWAP!" << endl;
+    int temp = arr[end];
+    arr[end] = arr[start];
+    arr[start] = temp;
   }
+  if (end-start+1 > 2){ //set 2/3 point
+    int m = ceil((end-start+1)/3);
+    stoogeSort(arr,start,end-m);  //run on lower 2/3
+    stoogeSort(arr,start+m,end);  //run on upper 2/3
+    stoogeSort(arr,start,end-m);  //run on lower 2/3 (again)
+  }
+}
